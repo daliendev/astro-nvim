b6c6194d99cbc40c507cc826cb8dc8760b3b36e8
daliendev <daniel.cadeau@proton.me>
2024-09-24 00:44:22 +0200

    (feat): vim api code blocks to enforce wrapping + disable autoformating

--- lua/lazy_setup.lua
+++ lua/lazy_setup.lua
@@ -42,3 +42,18 @@ require("lazy").setup({
     },
   },
 } --[[@as LazyConfig]])
+
+-- Auto-disable LSP formatting on save
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    client.server_capabilities.documentFormattingProvider = false
+  end,
+})
+
+-- Enable line wrapping globally
+vim.api.nvim_create_autocmd("BufReadPost", {
+  pattern = "*",
+  command = "set wrap",
+})
+
