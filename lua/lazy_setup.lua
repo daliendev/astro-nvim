928bba5b3c12c5f3b635b4a277ac348ce30b4b49
daniel cadeau <daniel.cadeau@proton.me>
2024-10-13 23:00:39 +0000

    feat: refresh neo-tree on Neogit status updates

--- lua/lazy_setup.lua
+++ lua/lazy_setup.lua
@@ -14,7 +14,7 @@ require("lazy").setup({
   },
   { import = "community" },
   { import = "plugins" },
-  -- Adding Neogit and dependencies here
+  -- Adding dependencies here
   {
     "NeogitOrg/neogit",
     dependencies = {
@@ -22,7 +22,19 @@ require("lazy").setup({
       "sindrets/diffview.nvim",        -- optional - Diff integration
       "nvim-telescope/telescope.nvim", -- optional
     },
-    config = true,
+    config = function ()
+      require("neogit").setup({})
+
+      -- Set up autocmd to refresh neo-tree after Neogit operations
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "NeogitStatusRefreshed",
+        callback = function()
+          require("neo-tree.sources.filesystem.commands").refresh(
+            require("neo-tree.sources.manager").get_state("filesystem")
+          ) -- Refresh neo-tree filesystem as expanded here https://github.com/nvim-neo-tree/neo-tree.nvim/discussions/1253
+        end,
+      })
+    end,
   },
   {
     "nvim-neo-tree/neo-tree.nvim",
