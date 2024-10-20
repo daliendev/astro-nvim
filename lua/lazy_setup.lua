c6d2f46a44ef0217805e2eff90f451c442d10422
dalien <daniel.cadeau@proton.me>
2024-10-20 17:53:10 +0000

    feat(nvim): improve syntax highlighting and indentation with nvim-treesitter; add autotag support

--- lua/lazy_setup.lua
+++ lua/lazy_setup.lua
@@ -117,16 +117,37 @@ require("lazy").setup({
   {
     -- nvim-treesitter setup
     "nvim-treesitter/nvim-treesitter",
-    run = ":TSUpdate",
+    build = ":TSUpdate",  -- Changed from 'run' to 'build' as per newer Lazy.nvim specs
     config = function()
       require("nvim-treesitter.configs").setup({
         ensure_installed = {
           "astro",
+          "html",
+          "css",
+          "javascript",
+          "typescript",
+          "tsx",
+          "lua",
+          "vim",
+          "markdown",
+          "markdown_inline",
         },
         highlight = {
           enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+        indent = {
+          enable = true,
         },
       })
+    end,
+  },
+  {
+    -- nvim-ts-autotag plugin for autoclose and autorename of tags
+    "windwp/nvim-ts-autotag",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    config = function()
+      require('nvim-ts-autotag').setup()
     end,
   },
 } --[[@as LazySpec]], {
