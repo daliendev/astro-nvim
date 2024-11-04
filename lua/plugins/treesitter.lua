fa50f76f48a2fdac0510ffd608463e3adf775d46
dalien <daniel.cadeau@proton.me>
2024-11-04 22:18:20 +0000

    refactor: treesitter config file

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,29 +1,25 @@
 ---@type LazySpec
 return {
-    -- nvim-treesitter setup
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",  -- Changed from 'run' to 'build' as per newer Lazy.nvim specs
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = {
-          "astro",
-          "html",
-          "css",
-          "javascript",
-          "typescript",
-          "tsx",
-          "lua",
-          "vim",
-          "markdown",
-          "markdown_inline",
-        },
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false,
-        },
-        indent = {
-          enable = true,
-        },
-      })
-    end,
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "astro",
+      "html",
+      "css",
+      "javascript",
+      "typescript",
+      "tsx",
+      "lua",
+      "vim",
+      "markdown",
+      "markdown_inline",
+    },
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = false,
+    },
+    indent = {
+      enable = true,
+    },
+  },
 }
